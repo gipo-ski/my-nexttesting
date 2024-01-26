6229c5e090887f175f5cfa909ade7a46869afe0d
gipo-ski <koladiji@gmail.com>
2024-01-26 19:30:52 +0000

    updated my-testting repo, added more tests to Home.test.tsx

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,14 +1,30 @@
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
-import Home from '@/app/page'
+import Home from "@/app/page";
 
-describe('Home', () => {
-  it('should have Docs text', () => {
-      render(<Home />) // ARRANGE
-      
-      const myElem = screen.getByText('Docs') //ACT
-      
-    expect(myElem).toBeInTheDocument() // ASSERT
-  })
-})
+describe("Home", () => {
+  it("should have a heading", () => {
+    render(<Home />); // ARRANGE
+
+    const myElem = screen.getByRole("heading", { name: 'Templates{" "}' }); //ACT
+
+    expect(myElem).toBeInTheDocument(); // ASSERT
+  });
+
+  it("should have Docs text", () => {
+    render(<Home />); // ARRANGE
+
+    const myElem = screen.getByText("Docs"); //ACT
+
+    expect(myElem).toBeInTheDocument(); // ASSERT
+  });
+
+  it('should contain the text "information"', () => {
+    render(<Home />); // ARRANGE
+
+    const myElem = screen.getByText(/information/i); //ACT
+
+    expect(myElem).toBeInTheDocument(); // ASSERT
+  });
+});
